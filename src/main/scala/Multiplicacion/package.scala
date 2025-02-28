8f4dd205164e9a591b0a9e86017093201240e93a
elkin <elkin.21.74@gmail.com>
2025-02-28 16:40:15 -0500

    finalizacion del punto uno

--- src/main/scala/Multiplicacion/package.scala
+++ src/main/scala/Multiplicacion/package.scala
@@ -1,3 +1,23 @@
+import javax.naming.spi.DirStateFactory.Result
+import scala.annotation.tailrec
+
 package object Multiplicacion {
+  def PeasantAlgorithm (a:Int, b:Int): Int = {
+    def PeasantAlgorithmRecursiva(a:Int, b:Int): Int = {
+      if (a == 1) b else
+        if (a%2 != 0) b + PeasantAlgorithmRecursiva(a / 2, b + b)
+        else PeasantAlgorithmRecursiva(a / 2, b + b)
+    }
+    PeasantAlgorithmRecursiva(a:Int, b:Int)
+  }
+
+  def PeasantAlgorithmIt (x:Int, y:Int): Int = {
+    def PeasantAlgorithmRev(x:Int, y:Int, r:Int): Int = {
+      if (x == 1) y+r else
+        if (x%2 != 0) PeasantAlgorithmRev(x/2, y + y, r + y)
+        else PeasantAlgorithmRev(x/2, y + y, r)
+    }
+    PeasantAlgorithmRev(x, y, 0)
+  }
 
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,2 +1,13 @@
+import Multiplicacion.{PeasantAlgorithm, PeasantAlgorithmIt}
 
+PeasantAlgorithm(74, 36)
+PeasantAlgorithm(10, 100)
+PeasantAlgorithm(999, 2)
+PeasantAlgorithm(2, 999)
+PeasantAlgorithm(1, 500000)
 
+PeasantAlgorithmIt(74, 36)
+PeasantAlgorithmIt(10, 100)
+PeasantAlgorithmIt(999, 2)
+PeasantAlgorithmIt(2, 999)
+PeasantAlgorithmIt(1, 500000)
