9da69e60fb348801edefb14eea664563d070ea43
elkin <elkin.21.74@gmail.com>
2025-03-02 14:46:01 -0500

    Finalizacion del taller

--- src/main/scala/Multiplicacion/package.scala
+++ src/main/scala/Multiplicacion/package.scala
@@ -1,6 +1,3 @@
-import javax.naming.spi.DirStateFactory.Result
-import scala.annotation.tailrec
-
 package object Multiplicacion {
   def PeasantAlgorithm (a:Int, b:Int): Int = {
     def PeasantAlgorithmRecursiva(a:Int, b:Int): Int = {
@@ -20,4 +17,67 @@ package object Multiplicacion {
     PeasantAlgorithmRev(x, y, 0)
   }
 
+  def splitMultiply(a:Int, b:Int): Int = {
+
+    def maxAB(a: Int, b: Int): Int = {
+      if (a >= b) a else b
+    }
+    def countDig(a:Int, count:Int): Int = {
+      if (a<10) count else countDig(a/10, count + 1)
+    }
+
+    def splitMuliplyR(a:Int, b:Int):Int = {
+
+      if (a < 10 && b < 10)
+        a*b
+      else
+        val m: Int = maxAB(countDig(a, 1), countDig(b, 1))/2
+        val pow: Int = math.pow(10, m).toInt
+
+        val x: Int = a / pow
+        val y: Int = a % pow
+        val z: Int = b / pow
+        val w: Int = b % pow
+
+        val p1: Int = splitMuliplyR(x, z)
+        val p2: Int = splitMuliplyR(y, z)
+        val p3: Int = splitMuliplyR(x, w)
+        val p4: Int = splitMuliplyR(y, w)
+
+        p1 * Math.pow(10, 2 * m).toInt + (p2 + p3) * pow + p4
+    }
+    splitMuliplyR(a:Int,b:Int)
+  }
+
+  def FastAlgorithm(a: Int, b: Int): Int = {
+
+    def maxAB(a: Int, b: Int): Int = {
+      if (a >= b) a else b
+    }
+
+    def countDig(a: Int, count: Int): Int = {
+      if (a < 10) count else countDig(a / 10, count + 1)
+    }
+
+    def FastAlgorithmR(a: Int, b: Int): Int = {
+      if (a < 10 && b < 10)
+        a * b
+      else
+        val m: Int = maxAB(countDig(a, 1), countDig(b, 1)) / 2
+        val pow: Int = math.pow(10, m).toInt
+
+        val x: Int = a / pow
+        val y: Int = a % pow
+        val z: Int = b / pow
+        val w: Int = b % pow
+
+        val p1: Int = FastAlgorithmR(x, z)
+        val p2: Int = FastAlgorithmR(y, w)
+        val p3: Int = FastAlgorithmR(x + y, z + w) - p1 - p2
+
+        p1 * Math.pow(10, 2 * m).toInt + p3 * pow + p2
+    }
+
+    FastAlgorithmR(a: Int, b: Int)
+  }
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,4 +1,4 @@
-import Multiplicacion.{PeasantAlgorithm, PeasantAlgorithmIt}
+import Multiplicacion.{FastAlgorithm, PeasantAlgorithm, PeasantAlgorithmIt, splitMultiply}
 
 PeasantAlgorithm(74, 36)
 PeasantAlgorithm(10, 100)
@@ -11,3 +11,17 @@ PeasantAlgorithmIt(10, 100)
 PeasantAlgorithmIt(999, 2)
 PeasantAlgorithmIt(2, 999)
 PeasantAlgorithmIt(1, 500000)
+
+splitMultiply(1500,2000)
+splitMultiply(74, 36)
+splitMultiply(10, 100)
+splitMultiply(999, 2)
+splitMultiply(2, 999)
+splitMultiply(1, 500000)
+
+FastAlgorithm(1500,2000)
+FastAlgorithm(74, 36)
+FastAlgorithm(10, 100)
+FastAlgorithm(999, 2)
+FastAlgorithm(2, 999)
+FastAlgorithm(1, 500000)
